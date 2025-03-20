2957092b10ab5f983195ca7d37a0fa6c24a54f20
Faycel2015 <faycel.ayech@gmail.com>
2025-03-20 18:14:28 +0100

    Initial commit

---
+++ CalTrack/CalTrack/CalTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalTrackApp.swift
+//  CalTrack
+//
+//  Created by FayTek on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct CalTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
